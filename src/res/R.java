2c5ce9b8e3ae323be5ecd551d1f054aeedeaa4b6
KateTroshkova <e-troshkova@inbox.ru>
2019-04-13 11:25:36 +0700

    create res directory and Resources class to hold string resources, add application icon

---
+++ src/res/R.java
@@ -0,0 +1,8 @@
+package res;
+
+public class R {
+
+    public static class image{
+        public static final String ic_launcher="/res/drawable/icon.png";
+    }
+}
--- src/view/Main.java
+++ src/view/Main.java
@@ -4,15 +4,18 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import res.R;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("CAMEROLL");
+        primaryStage.setTitle("cameroll");
+        primaryStage.getIcons().add(new Image(R.image.ic_launcher));
         Scene mainScene=new Scene(root, 1600, 800);
         FlowPane grid=(FlowPane)mainScene.lookup("#grid_parent");
         primaryStage.setScene(mainScene);
