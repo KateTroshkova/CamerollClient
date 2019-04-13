2cc80f5f972207270b6d213c1c3a647ad3bac44a
KateTroshkova <e-troshkova@inbox.ru>
2019-04-13 11:11:30 +0700

    choose flow pane as scene parent

--- src/view/Main.java
+++ src/view/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -11,11 +12,12 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("main.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CAMEROLL");
+        Scene mainScene=new Scene(root, 1600, 800);
+        FlowPane grid=(FlowPane)mainScene.lookup("#grid_parent");
+        primaryStage.setScene(mainScene);
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
