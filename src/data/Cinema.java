e3ed23badc6f9030b097c73e9caea94a69c5eecb
KateTroshkova <e-troshkova@inbox.ru>
2019-04-15 23:46:49 +0700

    define cinema class methods

--- src/data/Cinema.java
+++ src/data/Cinema.java
@@ -1,10 +1,52 @@
 package data;
 
+import javafx.scene.image.Image;
+
 import java.io.Serializable;
 
-public class Cinema implements Serializable {
+/*
+bd version
+id - autoincrement integer
+name - text
+address - text
+ */
+
+/*
+Связан с Сеансом
+ */
+
+public class Cinema implements Serializable, IRandomShow {
 
     private long id;
     private String name;
     private String address;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
 }
