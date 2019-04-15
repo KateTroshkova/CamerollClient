97451a1dd2a8ebdb70125dffbab28bf12e739d4f
KateTroshkova <e-troshkova@inbox.ru>
2019-04-15 23:42:21 +0700

    define place class methods, create enum to hold place status"

--- src/data/Place.java
+++ src/data/Place.java
@@ -3,8 +3,55 @@ package data;
 public class Place{
     private int x;
     private int y;
-    private boolean isExist;
-    private boolean isBooked;
-    private boolean isTaken;
-    private boolean isFree;
+    private PLACE_STATUS status;
+
+    public Place(int x, int y, PLACE_STATUS status){
+        this.x=x;
+        this.y=y;
+        this.status=status;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public boolean isExist(){
+        return true;
+    }
+
+    public boolean isTaken(){
+        return false;
+    }
+
+    public boolean isBooked(){
+        return false;
+    }
+
+    public boolean isFree(){
+        return true;
+    }
+
+    public void setTaken(){
+
+    }
+
+    public void setBooked(){
+
+    }
+
+    public void setFree(){
+
+    }
 }
