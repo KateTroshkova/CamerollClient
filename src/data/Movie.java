1638188d9e835e0ed55aa45bee84d08639cb5b43
KateTroshkova <e-troshkova@inbox.ru>
2019-04-15 23:17:11 +0700

    define db look for movie entity, create interface to generate random images

--- src/data/Movie.java
+++ src/data/Movie.java
@@ -1,14 +1,84 @@
 package data;
 
+import javafx.scene.image.Image;
+
 import java.io.Serializable;
 
-public class Movie implements Serializable {
+/*
+bd version
+id - autoincrement integer
+name - text
+description - text
+actors - text
+year - integer
+country - text
+ */
+
+/*
+Связан с Сеансом
+Поля используются только для отображения на экране
+Пустой конструктор для десериализации json
+ */
+
+public class Movie implements Serializable, IRandomShow {
 
     private long id;
     private String name;
     private String description;
     private String actors;
-    private String year;
+    private int year;
     private String country;
 
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getActors() {
+        return actors;
+    }
+
+    public void setActors(String actors) {
+        this.actors = actors;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    @Override
+    public Image getImage() {
+        return null;
+    }
 }
